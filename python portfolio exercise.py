26800e3a9326f7fc197ae15c04d865b41ec5a868
RossanKheed <nassor.m.majid@gmail.com>
2026-06-03 19:35:55 +0300

    Create python portfolio exercise.py
    ​"This repository serves as a collection of my early Python programming projects. Each script is designed to practice core programming concepts such as conditional logic, mathematical computations, and user input handling. Through these exercises, I am building the logical foundation necessary for my journey into software engineering and computer science."

---
+++ python portfolio exercise.py
@@ -0,0 +1,8 @@
+# Scholarship criteria. 
+print("==============SCHOLARSHIP CRITERIA AND ITS ELIGIBILITY ==============")
+physics_grade=int(input("What is you physics grade:"))
+math_grade=int(input("What is your math grade:"))
+if physics_grade >=80 or math_grade >=80:
+    print("YOU ARE ELIGIBLE FOR THIS SCHOLARSHIP ")
+else:
+    print("YOU ARE NOT ELIGIBLE ")
