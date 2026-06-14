3e78fb6f9328c33e55ba34906bcad7601a1d1bbd
RossanKheed <nassor.m.majid@gmail.com>
2026-06-14 20:27:57 +0300

    Update distance calculation .py

--- distance calculation .py
+++ distance calculation .py
@@ -5,5 +5,6 @@ x2=float(input("Enter x2:"))
 y1=float(input("Enter y1:"))
 y2=float(input("Enter y2:"))
 distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
 #print the distance 
-print("The distance is",  distance)
+print(f"The distance is {distance}")
