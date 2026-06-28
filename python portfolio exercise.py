a703dea433e8d5104c1961e4a47afdee93aa69e7
RossanKheed <nassor.m.majid@gmail.com>
2026-06-28 09:57:09 +0300

    Update python portfolio exercise.py

--- python portfolio exercise.py
+++ python portfolio exercise.py
@@ -3,8 +3,8 @@ print("==============SCHOLARSHIP CRITERIA AND ITS ELIGIBILITY ==============")
 name = ("What is your name :")
 physics_grade=int(input("What is you physics grade:"))
 math_grade=int(input("What is your math grade:"))
-# if get either 80 or greater in physics or math then you are eligible. 
-if physics_grade >=80 or math_grade >=80:
+# if get either 80 and greater in physics or math then you are eligible. 
+if physics_grade >=80 and math_grade >=80:
     print(f"YOU ARE ELIGIBLE FOR THIS SCHOLARSHIP {name}")
 else:
     print("YOU ARE NOT ELIGIBLE ")
